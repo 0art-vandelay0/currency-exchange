0c682782d3764c0deadf398b00313943484b7586
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-30 10:39:51 -0700

    Add converCurrency static method

--- src/currency.js
+++ src/currency.js
@@ -1,8 +1,7 @@
 export default class Currency {
-    
     static async getUsdConversions() {
         try {
-            let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+            const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
             const jsonifiedResponse = await response.json();
             console.log(jsonifiedResponse.conversion_rates);
             if (!response.ok) {
@@ -16,4 +15,17 @@ export default class Currency {
         }
     }
 
+    static async convertCurrency(response, country, usd) {
+        try {
+            if (response.conversion_rates) {
+                const currency = response.conversion_rates[country];
+                const conversion = (currency * usd).toFixed(2);
+                return conversion;
+            } else {
+                throw new Error(response);
+            }
+        } catch (error) {
+            throw new Error(`There was an error handling your exchange request for ${country}. Error: ${error}`);
+        }
+    }
 }
