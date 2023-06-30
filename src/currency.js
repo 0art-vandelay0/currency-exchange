dfafc9b2550444bdb9041727815e50725326e882
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-30 10:29:12 -0700

    Update static method to return USD conversions

--- src/currency.js
+++ src/currency.js
@@ -1,16 +1,19 @@
 export default class Currency {
-    static async getCurrencyByCountry(country) {
+    
+    static async getUsdConversions() {
         try {
-            let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${country}`);
+            let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
             const jsonifiedResponse = await response.json();
+            console.log(jsonifiedResponse.conversion_rates);
             if (!response.ok) {
                 const errorMessage = `${response.status} ${response.statusText}
-                ${jsonifiedResponse.message}`;
+                    ${jsonifiedResponse.message}`;
                 throw new Error(errorMessage);
             }
             return jsonifiedResponse;
-        } catch(error) {
+        } catch (error) {
             return error;
         }
     }
+
 }
