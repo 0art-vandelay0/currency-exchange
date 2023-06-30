1e2a006a8f4852f5ece527e190b92a8a6488ea87
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-30 12:18:48 -0700

    Add branching conditional for response

--- src/index.js
+++ src/index.js
@@ -27,7 +27,20 @@ function getUsdConversions(country, usd) {
 
 
 function displayConversion(country, usd, conversion) {
-    document.getElementById("response").innerHTML = `${usd} USD = ${conversion} ${country}`;
+    const percentage = ((usd / conversion) * 100).toFixed(0);
+    document.getElementById("response").innerHTML = `${usd} USD = ${conversion} ${country}<br>
+    That's about ${percentage}% up against the USD.`;
+    if (percentage > 100) {
+        document.getElementById("response").innerHTML += `<br>That's more expensive!`;
+    } else if (percentage < 100 && percentage > 75) {
+        document.getElementById("response").innerHTML += `<br>That's a little cheaper.`;
+    } else if (percentage <= 75 && percentage > 50) {
+        document.getElementById("response").innerHTML += `<br>That's a lot cheaper!`;
+    } else if (percentage <= 50){
+        document.getElementById("response").innerHTML += `<br>Damn, that will go a lot further!`;
+    } else {
+        document.getElementById("response").innerHTML += `<br>That's about the same.`;
+    }
 }
 
 function printError(country, error) {
