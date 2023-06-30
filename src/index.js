e494f7e5e2327e80987c5ad90661485523027a95
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-30 08:54:06 -0700

    Stage starter business and UI logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Currency from './currency.js';
+
+// Business Logic
+
+function getCurrencyByCountry(country) {
+    Currency.getCurrencyByCountry(country)
+        .then(function(response) {
+            if (response.conversion_rates) {
+                displayResults(response, country);
+            } else {
+                printError(response, country);
+            }
+        });
+}
+
+
+// UI Logic
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+    const country = document.getElementById('exchange-choice').value;
+    getCurrencyByCountry(country);
+}
+
+window.addEventListener('load', function() {
+    document.getElementById('currency-form').addEventListener('submit', handleFormSubmit);
+});
